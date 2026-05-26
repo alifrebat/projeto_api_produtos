aa29679980dabdf60c29c0427165883ce9d987eb
alifrebat <alifrebat@gmail.com>
2026-05-26 15:21:19 -0300

    ALTERADO O ARQUIVO produto_router.py

--- routes/produto_route.py
+++ routes/produto_route.py
@@ -1,4 +1,44 @@
+#IMPORTAÇÃO DAS BIBLIOTECAS
 from fastapi import APIRouter
 from sqlalchemy import Session 
 
+#IMPORTAÇÃO DOS ARQUVIOS DO PROJETO
 from database import SessionLocal
+from controllers.produto_controller import ProdutoController
+from schemas.produto_schema import ProdutoSchema
+
+router = APIRouter(
+    prefix = "/produtos",
+    tags = ["Produtos"]
+)
+
+controller = ProdutoController()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/")
+def listar():
+    db = next(get_db)
+    return controller.listar(db)
+
+@router.post("/")
+def cadastrar(produto: ProdutoSchema):
+    db = next(get_db)
+    return controller.cadastrar(db, produto)
+
+@router.delete("/{id}")
+def excluir(id: int):
+    db = next(get_db)
+    return controller.excluir(db, id)
+
+@router.put("/{id}")
+def alterar(id, produto: ProdutoSchema):
+    db = next(get_db)
+    return controller.alterar(db, id, produto)
+
